1a2f58eb6bd687be486822304fec72c2e5a81ca3
Caio <caio.vr12@gmail.com>
2025-10-07 10:40:47 -0300

    subindo projeto spring boot sem alterações

---
+++ src/main/java/com/example/bombacombustivel/BombacombustivelApplication.java
@@ -0,0 +1,13 @@
+package com.example.bombacombustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombacombustivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombacombustivelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bombacombustivel/BombacombustivelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bombacombustivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombacombustivelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
